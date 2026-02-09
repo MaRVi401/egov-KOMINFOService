f8ff267377e6c546f4e6a8b2611d0bd38fdfaea9
Ahmad Yassin <ahmadyassin4521@gmail.com>
2026-02-09 09:33:44 +0700

    modifed app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title', 'E-Gov Kominfo')</title>
 
-    <link rel="icon" type="image/png" href="{{ asset('assets/images/landingPages/logo-kabSubang.webp') }}">
+    <link rel="icon" type="image" href="{{ asset('assets/images/landingPages/logo-kabSubang.webp') }}">
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     
