640a6cdcf1d6ca9c2a3c8d3edaa6a4f1af58888a
Ahmad Yassin <ahmadyassin4521@gmail.com>
2026-02-09 14:41:43 +0700

    modifed icon dashboard penggunaasn

--- routes/web.php
+++ routes/web.php
@@ -51,12 +51,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])
         ->name('dashboard');
 
-    Route::get('/roles-management', function () {
-        return view('pages.super-admin.roles-management.index');
-    })->name('roles-management');
-
-
     // Proses Logout
     Route::post('/logout', [LoginController::class, 'logout'])
         ->name('logout');
+
+    // Roles Management (Super Admin Only)
+    Route::get('/roles-management', function () {
+        return view('pages.super-admin.roles-management.index');
+    })->name('roles-management');
 });
