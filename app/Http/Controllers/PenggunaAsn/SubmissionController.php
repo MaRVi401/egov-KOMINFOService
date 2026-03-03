60917e73edc7bb717ebc585b53adedca837aea90
AwilGarut <lesmanapahril@gmail.com>
2026-03-03 13:29:29 +0700

    dashboard2kabi9d

--- app/Http/Controllers/PenggunaAsn/SubmissionController.php
+++ app/Http/Controllers/PenggunaAsn/SubmissionController.php
@@ -23,7 +23,7 @@ class SubmissionController extends Controller
         } else {
             $query->where('status', '!=', 'selesai');
         }
- 
+
         if ($request->has('search')) {
             $search = $request->search;
             $query->where(function ($q) use ($search) {
@@ -53,7 +53,7 @@ class SubmissionController extends Controller
         ->where('users_id', Auth::user()->uuid)
         ->firstOrFail();
 
-        $kategoriEmail = null; 
+        $kategoriEmail = null;
 
         if ($ticket->detailEmailGov) {
             if (!empty($ticket->detailEmailGov->pd_jenis_layanan) || !empty($ticket->detailEmailGov->pd_instansi_nama)) {
@@ -72,15 +72,15 @@ class SubmissionController extends Controller
             ->where('users_id', Auth::user()->uuid)
             ->firstOrFail();
 
-        
+
         if ($ticket->lampiran) {
-           
+
             if (Storage::disk('s3')->exists($ticket->lampiran)) {
                 Storage::disk('s3')->delete($ticket->lampiran);
             }
         }
 
-       
+
         $ticket->delete();
 
         return redirect()->route('submission.index')->with('success', 'Tiket dan lampiran dokumen berhasil dihapus.');
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,7 @@ class DatabaseSeeder extends Seeder
             ServiceSeeder::class,
             // LayananSeeder::class
             TiketSeeder::class,
-            DashboardKabidSeeder::class,
+            DashboardKabidSeeder::class
         ]);
     }
 }
