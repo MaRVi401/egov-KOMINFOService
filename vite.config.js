450aa25ebbe1fbb27bce099e060d4faedbbe1516
Ahmad Yassin <ahmadyassin4521@gmail.com>
2026-03-04 09:32:28 +0700

    update vite.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
+import { glob } from 'glob';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css',
-                    'resources/js/app.js',
-                    'resources/js/user-management.js',
-                    'resources/js/email-e-gov.js',
-                    'resources/js/profile.js',
-                    'resource/js/subdomain-forms.js',
-                    'resource/js/app-creation.js',
-                    'resource/js/compliant-forms.js',
-                    'resources/js/dashboard-kabid.js'
+                    ...glob.sync('resources/js/*.js'),
                     ],
             refresh: true,
         }),
